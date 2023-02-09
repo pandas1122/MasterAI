6de2d47f8c9f42cd94d3dde12402089b676ea3b2
pandas1122 <76550707+pandas1122@users.noreply.github.com>
2023-02-09 22:56:26 +0530

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-openai.api_key = "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"
-twilio_account_sid = "ACe964526165f5440f62bb8c9a94e5c561"
-twilio_auth_token = "509fdd5c354154b68a7b285e1a177468"
-twilio_number = "+13134640406r"
